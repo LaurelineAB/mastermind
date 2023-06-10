3bd3b3de42cf3b5e5aba8e49aa422b8bfc8c4542
LaurelineAB <laureline.agabibrac@gmail.com>
2023-06-10 22:46:59 +0000

    Couleurs définies

---
+++ index.js
@@ -0,0 +1,48 @@
+window.addEventListener("DOMContentLoader", function() {
+    
+    let colors =
+    [
+        {
+            id: 0,
+            name: "red",
+            hex: "#fc0000"
+        },
+        {
+            id: 1,
+            name: "orange",
+            hex: "#f95f00"
+        },
+        {
+            id: 2,
+            name: "yellow",
+            hex: "#f7ef00"
+        },
+        {
+            id: 3,
+            name: "green",
+            hex: "#13fc02"
+        },
+        {
+            id: 4,
+            name: "teal",
+            hex: "#02f9cc"
+        },
+        {
+            id: 5,
+            name: "blue",
+            hex: "#0227f7"
+        },
+        {
+            id: 6,
+            name: "purple",
+            hex: "#ac02f4"
+        },
+        {
+            id: 7,
+            name: "pink",
+            hex: "#f202b6"
+        },
+        
+    ]
+    
+})
